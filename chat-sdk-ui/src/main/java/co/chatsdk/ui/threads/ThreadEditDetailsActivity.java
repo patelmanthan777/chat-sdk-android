565a90397d05ed5b6b07fd3644c34046c3ed14ed
Pepe Becker <mail@pepebecker.com>
2018-09-13 18:35:28 +0700

    Allow setting custom image for group chat (in development)

--- chat-sdk-ui/src/main/java/co/chatsdk/ui/threads/ThreadEditDetailsActivity.java
+++ chat-sdk-ui/src/main/java/co/chatsdk/ui/threads/ThreadEditDetailsActivity.java
@@ -7,6 +7,10 @@
 
 package co.chatsdk.ui.threads;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,16 +21,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.io.File;
+
+import co.chatsdk.core.dao.Keys;
 import co.chatsdk.core.dao.Thread;
 import co.chatsdk.core.dao.ThreadMetaValue;
 import co.chatsdk.core.session.ChatSDK;
 import co.chatsdk.core.session.InterfaceManager;
 import co.chatsdk.core.session.StorageManager;
 import co.chatsdk.core.utils.DisposableList;
+import co.chatsdk.core.utils.ImageUtils;
 import co.chatsdk.core.utils.Strings;
 import co.chatsdk.ui.R;
+import co.chatsdk.ui.chat.MediaSelector;
 import co.chatsdk.ui.main.BaseActivity;
 import co.chatsdk.ui.utils.ToastHelper;
+import id.zelory.compressor.Compressor;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 
 /**
@@ -37,11 +49,15 @@ public class ThreadEditDetailsActivity extends BaseActivity {
     /** Set true if you want slide down animation for this context exit. */
     protected boolean animateExit = false;
 
+    protected MediaSelector mediaSelector = new MediaSelector();
     protected DisposableList disposableList = new DisposableList();
 
     protected ActionBar actionBar;
     protected String threadEntityID;
     protected Thread thread;
+    protected String avatarURL;
+
+    protected SimpleDraweeView avatarImageView;
     protected EditText nameInput;
     protected Button saveButton;
 
@@ -62,8 +78,28 @@ public class ThreadEditDetailsActivity extends BaseActivity {
         actionBar = getSupportActionBar();
         actionBar.setHomeButtonEnabled(true);
 
+        avatarImageView = findViewById(R.id.chat_sdk_edit_thread_iv);
         nameInput = findViewById(R.id.chat_sdk_edit_thread_name_et);
         saveButton = findViewById(R.id.chat_sdk_edit_thread_update_b);
+
+        avatarImageView.setOnClickListener(view -> mediaSelector.startChooseImageActivity(ThreadEditDetailsActivity.this, MediaSelector.CropType.Circle, result -> {
+            try {
+                File compress = new Compressor(ChatSDK.shared().context())
+                        .setMaxHeight(ChatSDK.config().imageMaxThumbnailDimension)
+                        .setMaxWidth(ChatSDK.config().imageMaxThumbnailDimension)
+                        .compressToFile(new File(result));
+
+                Bitmap bitmap = BitmapFactory.decodeFile(compress.getPath());
+
+                File file = ImageUtils.compressToFile(ChatSDK.shared().context(), bitmap, ChatSDK.currentUser().getEntityID(), ".png");
+                avatarURL = Uri.fromFile(file).toString();
+                avatarImageView.setImageURI(avatarURL);
+            }
+            catch (Exception e) {
+                ChatSDK.logError(e);
+                Toast.makeText(ThreadEditDetailsActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }));
 
         if (thread != null) {
             actionBar.setTitle(Strings.nameForThread(thread));
@@ -71,9 +107,13 @@ public class ThreadEditDetailsActivity extends BaseActivity {
             saveButton.setText(R.string.update_thread);
 
             // TODO: permanently move thread name into meta data
-            ThreadMetaValue nameMetaValue = thread.metaValueForKey("name");
+            ThreadMetaValue nameMetaValue = thread.metaValueForKey(Keys.Name);
             if (nameMetaValue != null)
                 nameInput.setText(nameMetaValue.getValue());
+
+            ThreadMetaValue avatarMetaValue = thread.metaValueForKey(Keys.AvatarURL);
+            if (avatarMetaValue != null)
+                avatarImageView.setImageURI(avatarMetaValue.getValue());
         } else {
             saveButton.setEnabled(false);
         }
@@ -108,15 +148,16 @@ public class ThreadEditDetailsActivity extends BaseActivity {
 
             disposableList.add(ChatSDK.publicThread().createPublicThreadWithName(threadName)
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe((_thread, throwable) -> {
+                    .subscribe((newThread, throwable) -> {
                         if (throwable == null) {
                             // TODO: permanently move thread name into meta data
-                            thread.setMetaValue("name", threadName);
-                            disposableList.add(ChatSDK.thread().pushThreadMeta(thread).subscribe(() -> {
+                            newThread.setMetaValue(Keys.Name, threadName);
+                            newThread.setMetaValue(Keys.AvatarURL, avatarURL);
+                            disposableList.add(ChatSDK.thread().pushThreadMeta(newThread).subscribe(() -> {
                                 dismissProgressDialog();
                                 ToastHelper.show(ChatSDK.shared().context(), String.format(getString(co.chatsdk.ui.R.string.public_thread__is_created), threadName));
 
-                                ChatSDK.ui().startChatActivityForID(ChatSDK.shared().context(), _thread.getEntityID());
+                                ChatSDK.ui().startChatActivityForID(ChatSDK.shared().context(), newThread.getEntityID());
                             }));
                         } else {
                             ChatSDK.logError(throwable);
@@ -128,7 +169,8 @@ public class ThreadEditDetailsActivity extends BaseActivity {
             // TODO: permanently move thread name into meta data
             thread.setName(threadName);
             thread.update();
-            thread.setMetaValue("name", threadName);
+            thread.setMetaValue(Keys.Name, threadName);
+            thread.setMetaValue(Keys.AvatarURL, avatarURL);
             // TODO: Update the thread name
             disposableList.add(ChatSDK.thread().pushThreadMeta(thread).subscribe(this::finish));
         }
@@ -156,6 +198,18 @@ public class ThreadEditDetailsActivity extends BaseActivity {
             onBackPressed();
         }
         return true;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            mediaSelector.handleResult(this, requestCode, resultCode, data);
+        }
+        catch (Exception e) {
+            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            ChatSDK.logError(e);
+        }
     }
 
 }
